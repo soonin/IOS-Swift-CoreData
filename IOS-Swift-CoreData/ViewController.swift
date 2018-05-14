7623c2bedae13ea1c75f24f15e10e3fedc0677ae
Pooya Hatami <pooya@node-1w7jr9qrok5tbmfbapha9d96d.ipv6.telus.net>
2018-05-14 13:36:58 -0700

    Initial Commit

---
+++ IOS-Swift-CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS-Swift-CoreData
+//
+//  Created by Pooya Hatami on 2018-05-14.
+//  Copyright © 2018 Pooya Hatami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
